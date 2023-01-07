23b00d018d2bb15a198dd93b24e939dd4e853c19
Njiraini23 <njiraini88@gmail.com>
2023-01-07 13:52:09 +0000

    formatting

---
+++ 2strings.py
@@ -0,0 +1,2 @@
+sammy_string = "Sammy loves {} {}, and has {} {}."                      #4 {} placeholders
+print(sammy_string.format("open-source", "software", 5, "balloons"))    #Pass 4 strings into method
---
+++ variables.py
@@ -0,0 +1,1 @@
+print("Sammy the {0} {1} a {pr}.".format("shark", "made", pr = "pull request"))
