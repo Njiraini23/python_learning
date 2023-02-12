186db2867ebd98c5568f5b4983c399e26d29d451
Njiraini23 <njiraini88@gmail.com>
2023-02-12 08:23:05 +0000

    Tuple

---
+++ tests/myTuple.py
@@ -0,0 +1,15 @@
+myTuple = (1, 2, 3, 4, 5)
+
+print("Ist value :", myTuple[0])
+
+print(myTuple[0:3])
+
+print("Tuple Length :", len(myTuple))
+
+moreFibs = myTuple + (13, 21, 34)
+
+print("32 in Tuple :", 34 in moreFibs)
+
+for i in moreFibs:
+    print(i)
+
