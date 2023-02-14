d8602eb1dda14b4c2f4a16b5613756365b8c99bd
Njiraini23 <njiraini88@gmail.com>
2023-02-14 13:57:54 +0000

    person.py

---
+++ tests/person.py
@@ -0,0 +1,31 @@
+#!usr/bin/env python3
+class Person:
+
+    def __init__(self, name, age, height, sex):
+        self.__name = name
+        self.__age = age
+        self.__height = height
+        self.__sex = sex
+
+    def talk(self):
+        print(f"Hi there you told me to talk")
+
+    def walk(self):
+        print(f"Just imagine I'm walking")
+
+    def __str__(self):
+        return f"This person is called {self.__name} and is {self.__age} years \
+and is {self.__height}cm tall"
+
+        
+def main():
+
+    person1 = Person("Tuva", 23, 178, "male")
+
+    print(person1)
+
+    person1.talk()
+
+    person1.walk()
+
+main()
