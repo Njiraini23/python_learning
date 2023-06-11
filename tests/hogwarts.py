54b1d0cb2b0ff00cf7087bca41275832b4801efc
Njiraini23 <njiraini88@gmail.com>
2023-06-11 13:51:26 +0000

    hogwarts4.py

---
+++ tests/hogwarts.py
@@ -0,0 +1,4 @@
+students = ["Ken", "Imondo", "Njorua", "Konyeki"]
+
+for i in range(len(students)):
+   print(i + 1 , students[i])
---
+++ tests/hogwarts2.py
@@ -0,0 +1,8 @@
+students = {
+        "Imondo": "Thagana",
+        "Jakwiri": "Kimathi",
+        "Jatelo": "Kimathi",
+        "Ron": "Thagana",
+        }
+for student in students:
+    print(student, students[student], sep=": ")
