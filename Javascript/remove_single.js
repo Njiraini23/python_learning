439f61a7d34b4d0fb3d57508f8a55047ab5994fe
Njiraini23 <njiraini88@gmail.com>
2023-08-21 12:02:47 +0000

    remove_single.js

---
+++ Javascript/remove_single.js
@@ -0,0 +1,6 @@
+const fruits = ["Strawberry", "Banana", "Mango"];
+const start = fruits.indexOf("Banana");
+const deleteCount = 1;
+const removedItems = fruits.splice(start, deleteCount);
+console.log(fruits);
+console.log(removedItems);
