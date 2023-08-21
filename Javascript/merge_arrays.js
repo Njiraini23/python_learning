a2e611d1f4ee21ea5350343748b959cfa82818a5
Njiraini23 <njiraini88@gmail.com>
2023-08-21 12:45:44 +0000

    merge_arrays.js

---
+++ Javascript/merge_arrays.js
@@ -0,0 +1,6 @@
+const fruits = ["Apple", "Banana", "Strawberry"];
+const moreFruits = ["Mango", "Cherry"];
+const combinedFruits = fruits.concat(moreFruits);
+console.log(combinedFruits);
+console.log(fruits);
+console.log(moreFruits);
