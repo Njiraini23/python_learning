c2769571e2b987a9c82a8990c0e01cbc7845a867
Njiraini23 <njiraini88@gmail.com>
2023-08-21 11:54:31 +0000

    multiple_removal.js

---
+++ Javascript/multiple_removal.js
@@ -0,0 +1,6 @@
+const fruits = ["Apple", "Strawberry", "Cherry", "Banana", "Mango"];
+const start = 0;
+const deleteCount = 3;
+const removedItems = fruits.splice(start, deleteCount);
+console.log(fruits);
+console.log(removedItems);
