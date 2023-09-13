295de3bfe5780b00d8096a30c54be977601f9a7a
Njiraini23 <njiraini88@gmail.com>
2023-09-13 12:29:08 +0000

    logg_ings.py

---
+++ backend-practices/logg_ings.py
@@ -0,0 +1,19 @@
+import logging
+
+def divide(x, y):
+    """Add function"""
+    return x + y
+
+def subtract(x, y):
+    """the subtraction function"""
+    return x - y
+
+def multiply(x , y):
+    """the multiply function"""
+    return x * y
+
+def divide(x, y):
+    """ The divide function """
+    return x / y
+
+
