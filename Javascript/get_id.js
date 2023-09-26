5fb69786fcce47a4538ba50c84ecac6b8356d29c
Njiraini23 <njiraini88@gmail.com>
2023-09-26 06:26:18 +0000

    get_id.js

---
+++ Javascript/get_id.js
@@ -0,0 +1,10 @@
+if (process.getgid && process.setgid) {
+  console.log('Current gid: ' + process.getgid());
+    try {
+      process.setgid(501);
+      console.log('New gid: ' + process.getgid());
+    }
+    catch (err) {
+      console.log('Failed to set gid: ' + err);
+    }
+}
