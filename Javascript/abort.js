d9083ac12952faa41b24e14667cf38282c2b5127
Njiraini23 <njiraini88@gmail.com>
2023-09-26 10:30:29 +0000

    abort.js

---
+++ Javascript/abort.js
@@ -0,0 +1,7 @@
+const { exec } = require('node:child_process');
+const controller = new AbortController();
+const { signal } = controller;
+const child = exec('grep ssh', { signal }, (error) => {
+  console.error(error); // an AbortError
+});
+controller.abort();
