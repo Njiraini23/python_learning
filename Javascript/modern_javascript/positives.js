ae9583225e63046eb47095bafa6a129dc0359c59
Njiraini23 <njiraini88@gmail.com>
2023-10-03 23:26:42 +0000

    positives.js

--- Javascript/modern_javascript/positives.js
+++ Javascript/modern_javascript/positives.js
@@ -1,5 +1,7 @@
 const numbers = [2, -30, 50, 20, -12, -9, 7];
 
-const positiveSum = numbers.filter((number) => number > 0 );
+const positiveSum = numbers
+	.filter((number) => number > 0)
+	.reduce((acc, cur) => acc + cur, 0);
 
 console.log(positiveSum);
