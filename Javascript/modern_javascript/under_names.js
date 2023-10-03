b2dcc82ad4a955b19920c712d869d24c00e74510
Njiraini23 <njiraini88@gmail.com>
2023-10-03 21:08:34 +0000

    under_names.js

---
+++ Javascript/modern_javascript/under_names.js
@@ -0,0 +1,41 @@
+const people = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john@gmail.com',
+    phone: '111-111-111',
+    age: 30,
+  },
+  {      
+    firstName: 'Jane',
+    lastName: 'Poe',
+    email: 'jane@gmail.com',
+    phone: '222-222-222',
+    age: 25,
+  },
+  {      
+    firstName: 'Bob',
+    lastName: 'Foe',
+    email: 'bob@gmail.com',
+    phone: '333-333-333',
+    age: 45,
+  },     
+  {      
+    firstName: 'Sara',
+    lastName: 'Soe',
+    email: 'sara@gmail.com',
+    phone: '444-444-444',
+    age: 19,
+  },      
+  {      
+    firstName: 'Jose',
+    lastName: 'Koe',
+    email: 'jose@gmail.com',
+    phone: '555-555-555',
+    age: 23,
+  },   
+];
+
+const youngPeople = people.filter((person) => {
+  person.age <= 25);
+console.log(youngPeople);
