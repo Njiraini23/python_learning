f05164f1a2def6a8f2e6c929c2f64c8defd84459
Njiraini23 <njiraini88@gmail.com>
2023-10-11 11:24:52 +0000

    express.js

--- Javascript/modern_javascript/express.js
+++ Javascript/modern_javascript/express.js
@@ -1,1 +1,15 @@
-1
+const express = require('express');
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).send('Homepage')
+})
+
+app.get('/About',() => {
+  res.status(200).send('About Us')
+})
+
+app.listen(5000, (req, res) => {
+  console.log('Listening to port 5000');
+})
