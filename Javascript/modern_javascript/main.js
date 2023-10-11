77676dd8f11d1fce5ff36718f703ad5bea6206f0
Njiraini23 <njiraini88@gmail.com>
2023-10-11 10:59:29 +0000

    main.js

--- Javascript/modern_javascript/main.js
+++ Javascript/modern_javascript/main.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.status(200).send('Homepage')
+})
+
+app.get('/about', () => {
+  res.status(200).send('About Page')
+})
+
+app.all('*', () => {
+  res.status(404).send('<h> Resource not Found </>')
+}) 
+
 app.listen(5000, () => {
   console.log('server is listening on port 5000');
 });
